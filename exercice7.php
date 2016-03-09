2425d98a019c8221e7f5fb84f57ab844c8ab0a0c
unknown <21056934@etu.unicaen.fr>
2016-03-09 18:31:30 +0100

    Fix bug on exercice7.php

--- exercice7.php
+++ exercice7.php
@@ -1,8 +1,8 @@
 <?php
 
-    if (isset($_GET['method']) && isset($_GET['content'])) {
-        $res = $_GET['method']($_GET['content']);
-    }
+if (isset($_GET['method']) && isset($_GET['content'])) {
+    $res = $_GET['method']($_GET['content']);
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -26,25 +26,25 @@
 <div>
     <form method="get" action="exercice7.php">
         <divclass="form-item">
-            <label>Frapper votre texte : </label><br />
-            <textarea name="content" class="form-item"><?php echo (isset($_GET['content']) ? $_GET['content'] : ''); ?></textarea>
-        </div>
-        <div>
-            <select name="method" multiple class="form-item">
-                <option value="ucfirst">1ère lettre en MAJ</option>
-                <option value="strtoupper">Majuscule</option>
-                <option value="strtolower">Miniscule</option>
-                <option value="charCount">ligne->BR</option>
-                <option value="sha1">sha1</option>
-                <option value="md5">md5</option>
-                <option value="ucwords">1ère lettre mot en MAJ</option>
-                <option value="urlencode">URL encode</option>
-                <option value="htmlentities">Entités HTML</option>
-                <option value="wordwrap">Césure chaine</option>
-            </select>
-        </div>
-        <input value="Appeler la fonction" type="submit" class="form-item"/>
-    </form>
+        <label>Frapper votre texte : </label><br />
+        <textarea name="content" class="form-item"><?php echo (isset($_GET['content']) ? $_GET['content'] : ''); ?></textarea>
+</div>
+<div>
+    <select name="method" multiple class="form-item">
+        <option value="ucfirst">1ère lettre en MAJ</option>
+        <option value="strtoupper">Majuscule</option>
+        <option value="strtolower">Miniscule</option>
+        <option value="nl2br">ligne->BR</option>
+        <option value="sha1">sha1</option>
+        <option value="md5">md5</option>
+        <option value="ucwords">1ère lettre mot en MAJ</option>
+        <option value="urlencode">URL encode</option>
+        <option value="htmlentities">Entités HTML</option>
+        <option value="wordwrap">Césure chaine</option>
+    </select>
+</div>
+<input value="Appeler la fonction" type="submit" class="form-item"/>
+</form>
 </div>
 <hr />
 <?php if(isset($res)) { ?>
