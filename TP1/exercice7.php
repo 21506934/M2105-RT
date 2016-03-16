a950e052bc51f42b747355182aea6231af0b9bcd
unknown <21056934@etu.unicaen.fr>
2016-03-16 14:23:55 +0100

    Re-Indent exercice7.php

--- TP1/exercice7.php
+++ TP1/exercice7.php
@@ -14,21 +14,21 @@ if (isset($_GET['method']) && isset($_GET['content'])) {
                 <label>Frapper votre texte : </label><br />
                 <textarea name="content" class="form-item"><?php echo (isset($_GET['content']) ? $_GET['content'] : ''); ?></textarea>
             </div>
-        <div>
-            <select name="method" multiple class="form-item">
-                <option value="ucfirst">1ère lettre en MAJ</option>
-                <option value="strtoupper">Majuscule</option>
-                <option value="strtolower">Miniscule</option>
-                <option value="nl2br">ligne->BR</option>
-                <option value="sha1">sha1</option>
-                <option value="md5">md5</option>
-                <option value="ucwords">1ère lettre mot en MAJ</option>
-                <option value="urlencode">URL encode</option>
-                <option value="htmlentities">Entités HTML</option>
-                <option value="wordwrap">Césure chaine</option>
-            </select>
-        </div>
-        <input value="Appeler la fonction" type="submit" class="form-item"/>
+            <div>
+                <select name="method" multiple class="form-item">
+                    <option value="ucfirst">1ère lettre en MAJ</option>
+                    <option value="strtoupper">Majuscule</option>
+                    <option value="strtolower">Miniscule</option>
+                    <option value="nl2br">ligne->BR</option>
+                    <option value="sha1">sha1</option>
+                    <option value="md5">md5</option>
+                    <option value="ucwords">1ère lettre mot en MAJ</option>
+                    <option value="urlencode">URL encode</option>
+                    <option value="htmlentities">Entités HTML</option>
+                    <option value="wordwrap">Césure chaine</option>
+                </select>
+            </div>
+            <input value="Appeler la fonction" type="submit" class="form-item"/>
         </form>
     </div>
     <hr />
